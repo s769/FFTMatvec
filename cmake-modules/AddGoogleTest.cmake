b01b7e1c3c491586e12f1308dfdd7b09b53d8170
Sreeram Venkat <srvenkat@utexas.edu>
2024-10-03 14:20:25 -0700

    MPI testing seems to work, test fixtures

--- cmake-modules/AddGoogleTest.cmake
+++ cmake-modules/AddGoogleTest.cmake
@@ -10,11 +10,11 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
     set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
 
     include(DownloadProject)
-    download_project(PROJ                googletest
-		     GIT_REPOSITORY      https://github.com/google/googletest.git
-		     GIT_TAG             v1.14.0
-		     UPDATE_DISCONNECTED 1
-		     QUIET
+    download_project(PROJ googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG v1.14.0
+        UPDATE_DISCONNECTED 1
+        QUIET
     )
 
     # CMake warning suppression will not be needed in version 1.9
@@ -24,7 +24,7 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
 else()
     include(FetchContent)
     FetchContent_Declare(googletest
-        GIT_REPOSITORY      https://github.com/google/googletest.git
+        GIT_REPOSITORY https://github.com/google/googletest.git
         GIT_TAG v1.14.0)
     FetchContent_GetProperties(googletest)
     if(NOT googletest_POPULATED)
@@ -37,11 +37,11 @@ endif()
 
 
 if(CMAKE_CONFIGURATION_TYPES)
-    add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} 
-        --force-new-ctest-process --output-on-failure 
+    add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND}
+        --force-new-ctest-process --output-on-failure
         --build-config "$<CONFIGURATION>")
 else()
-    add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} 
+    add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND}
         --force-new-ctest-process --output-on-failure)
 endif()
 set_target_properties(check PROPERTIES FOLDER "Scripts")
@@ -61,15 +61,15 @@ if(GOOGLE_TEST_INDIVIDUAL)
 endif()
 
 # Target must already exist
-macro(add_gtest TESTNAME FILES LIBRARIES)
+macro(add_gtest TESTNAME FILES LIBRARIES NPROCS)
     add_executable(${TESTNAME} ${FILES})
     target_link_libraries(${TESTNAME} PUBLIC gtest gmock gtest_main ${LIBRARIES})
     set_target_properties(${TESTNAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/Tests")
     if(GOOGLE_TEST_INDIVIDUAL)
         if(CMAKE_VERSION VERSION_LESS 3.10)
             gtest_add_tests(TARGET ${TESTNAME}
-                            TEST_PREFIX "${TESTNAME}."
-                            TEST_LIST TmpTestList)
+                TEST_PREFIX "${TESTNAME}."
+                TEST_LIST TmpTestList)
             set_tests_properties(${TmpTestList} PROPERTIES FOLDER "Tests")
         else()
             gtest_discover_tests(${TESTNAME}
@@ -81,18 +81,34 @@ macro(add_gtest TESTNAME FILES LIBRARIES)
         set_target_properties(${TESTNAME} PROPERTIES FOLDER "Tests")
     endif()
 
-endmacro()
 
-mark_as_advanced(
-gmock_build_tests
-gtest_build_samples
-gtest_build_tests
-gtest_disable_pthreads
-gtest_force_shared_crt
-gtest_hide_internal_symbols
-BUILD_GMOCK
-BUILD_GTEST
-)
 
-set_target_properties(gtest gtest_main gmock gmock_main
-    PROPERTIES FOLDER "Extern")
+    if(${NPROCS} LESS 1)
+        message(FATAL_ERROR "NPROCS must be greater than 0")
+    endif()
+    if(${NPROCS} GREATER 1)
+        if(NOT (DEFINED MPIEXEC_EXECUTABLE AND DEFINED MPIEXEC_NUMPROC_FLAG))
+            message(FATAL_ERROR "MPIEXEC_EXECUTABLE and MPIEXEC_NUMPROC_FLAG must be defined to use test_mpi_launcher")
+        endif()
+        if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.29)
+            set_property(TARGET ${TESTNAME} PROPERTY TEST_LAUNCHER ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NPROCS})
+        else()
+            set_property(TARGET ${TESTNAME} PROPERTY CROSSCOMPILING_EMULATOR ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NPROCS})
+        endif()
+        set_property(TEST ${TESTNAME} PROPERTY PROCESSORS ${NPROCS})
+    endif()
+    endmacro()
+
+    mark_as_advanced(
+        gmock_build_tests
+        gtest_build_samples
+        gtest_build_tests
+        gtest_disable_pthreads
+        gtest_force_shared_crt
+        gtest_hide_internal_symbols
+        BUILD_GMOCK
+        BUILD_GTEST
+    )
+
+    set_target_properties(gtest gtest_main gmock gmock_main
+        PROPERTIES FOLDER "Extern")
--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -1,21 +1,81 @@
-#include <gtest/gtest.h>
-#include "shared.hpp"
 #include "Comm.hpp"
+#include "gtest-mpi-listener.hpp"
+#include "shared.hpp"
+#include <gtest/gtest.h>
 
-TEST(UtilsTest, Example)
+int proc_rows, proc_cols;
+
+class UtilsTest : public ::testing::Test {
+protected:
+    static Comm* comm;
+    static void SetUpTestSuite()
+    {
+        if (comm == nullptr) {
+            comm = new Comm(MPI_COMM_WORLD, proc_rows, proc_cols);
+        }
+    }
+    static void TearDownTestSuite()
+    {
+        if (comm != nullptr) {
+            delete comm;
+            comm = nullptr;
+        }
+    }
+};
+
+Comm* UtilsTest::comm = nullptr;
+
+TEST_F(UtilsTest, Example)
 {
-    EXPECT_EQ(1, 1);
+    printf("proc_rows = %d, proc_cols = %d\n", proc_rows, proc_cols);
+    printf(
+        "comm proc_rows = %d, comm proc_cols = %d\n", comm->get_proc_rows(), comm->get_proc_cols());
+    ASSERT_EQ(comm->get_proc_rows(), proc_rows);
+    ASSERT_EQ(comm->get_proc_cols(), proc_cols);
 }
 
-// int main(int argc, char **argv)
-// {
-//     ::testing::InitGoogleTest(&argc, argv);
-//     MPICHECK(MPI_Init(&argc, &argv));
-//     int result;
-//     {
-//         Comm comm(MPI_COMM_WORLD, 2, 2);
-//         result = RUN_ALL_TESTS();
-//     }
-//     MPICHECK(MPI_Finalize());
-//     return result;
-// }
+TEST_F(UtilsTest, Example2)
+{
+    printf("proc_rows = %d, proc_cols = %d\n", proc_rows, proc_cols);
+    printf(
+        "comm proc_rows = %d, comm proc_cols = %d\n", comm->get_proc_rows(), comm->get_proc_cols());
+    ASSERT_EQ(comm->get_proc_rows(), proc_rows);
+    ASSERT_EQ(comm->get_proc_cols(), proc_cols);
+}
+
+int main(int argc, char** argv)
+{
+    // Filter out Google Test arguments
+    ::testing::InitGoogleTest(&argc, argv);
+
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    // Add object that will finalize MPI on exit; Google Test owns this pointer
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+
+    // Get the event listener list.
+    ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+
+    // Remove default listener: the default printer and the default XML printer
+    ::testing::TestEventListener* l = listeners.Release(listeners.default_result_printer());
+
+    // Adds MPI listener; Google Test owns this pointer
+    listeners.Append(new GTestMPIListener::MPIWrapperPrinter(l, MPI_COMM_WORLD));
+    // Run tests, then clean up and exit. RUN_ALL_TESTS() returns 0 if all tests
+    // pass and 1 if some test fails.
+
+    int world_size;
+    MPICHECK(MPI_Comm_size(MPI_COMM_WORLD, &world_size));
+    proc_cols = sqrt(world_size);
+    proc_rows = world_size / proc_cols;
+    if (proc_rows > proc_cols) {
+        int temp = proc_cols;
+        proc_cols = proc_rows;
+        proc_rows = temp;
+    }
+    printf("proc_rows = %d, proc_cols = %d\n", proc_rows, proc_cols);
+    int result = RUN_ALL_TESTS();
+
+    return result; // Run tests, then clean up and exit
+}
